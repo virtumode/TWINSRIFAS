a8ddf1fdd8b143fc524e3dde6f46c52bcbf32276
virtumode <virtumode@gmail.com>
2026-03-07 22:54:43 -0400

    Add files via upload

---
+++ env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="vite/client" />
+
+VITE_SUPABASE_URL = "https://qstxrzmbbnsimlgvexqo.supabase.co";
+VITE_SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzdHhyem1iYm5zaW1sZ3ZleHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0MzM1MzQsImV4cCI6MjA4MzAwOTUzNH0.ogs02Acan5LMouWoOdmqlO8Tpv1Tqn1gYWLP3JK8eWg";
